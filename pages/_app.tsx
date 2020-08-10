0658523b0bd47c4cadfa594a21e039f74e764071
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-08-10 02:08:10 -0400

    Prune starter boilerplate
    Start with a clean-ish slate:
    
    - Get rid of starter copy and styles
    - Get rid of css modules
    - Get rid of api/hello
    - Get rid of readme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
