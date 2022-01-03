8fba3c5b9b9e506767038bb0385f96287eb77cc7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-02 21:35:20 -0600

    refactor(header): for easier story writing

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,36 +7,7 @@ export const Header = () => {
   return (
     <>
       <header>
-        <nav>
-          {user && (
-            <>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-              <Link href="/authenticated">
-                <a>Authenticated page</a>
-              </Link>
-              <Link href="/gravity">
-                <a>Gravity example</a>
-              </Link>
-              <Link href="/metaphysics">
-                <a>Metaphysics example</a>
-              </Link>
-              <Link href="/artist-series">
-                <a>Artist series</a>
-              </Link>
-              <Link href="/api/logout">
-                <a>Logout</a>
-              </Link>
-            </>
-          )}
-
-          {!user && (
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          )}
-        </nav>
+        <Nav user={user} />
       </header>
 
       <style jsx>{`
@@ -44,12 +15,57 @@ export const Header = () => {
           background: black;
           color: white;
         }
+      `}</style>
+    </>
+  )
+}
+
+const Nav: React.FC<{ user: unknown }> = ({ user }) => {
+  return (
+    <>
+      <nav>
+        {user && (
+          <>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <Link href="/authenticated">
+              <a>Authenticated page</a>
+            </Link>
+            <Link href="/gravity">
+              <a>Gravity example</a>
+            </Link>
+            <Link href="/metaphysics">
+              <a>Metaphysics example</a>
+            </Link>
+            <Link href="/artist-series">
+              <a>Artist series</a>
+            </Link>
+            <Link href="/api/logout">
+              <a>Logout</a>
+            </Link>
+          </>
+        )}
+
+        {!user && (
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        )}
+      </nav>
+
+      <style jsx>{`
         nav {
           display: flex;
           justify-content: flex-end;
         }
         a {
-          padding: 1em;
+          padding: 1em 1.25em;
+          text-decoration: none;
+        }
+        a:hover {
+          background: #333;
+          text-decoration: underline;
         }
       `}</style>
     </>
